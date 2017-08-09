ff4c240d25bd2a656983b969a394c77b2327c181
rafaelatavares email = rafa-tavares@hotmail.com <Aluno0309@labf323459.fuvs.br>
2017-08-08 21:03:03 -0300

    Alteração .gitignore.

---
+++ HelloWorld/src/br/edu/univas/si4/es2_2017/HelloWorld.java
@@ -0,0 +1,11 @@
+package br.edu.univas.si4.es2_2017;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World !");
+	}
+
+}
